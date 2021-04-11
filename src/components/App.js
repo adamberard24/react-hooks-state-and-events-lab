cb3debaa120dad06d11a567b5bf8cea674762637
Adam Schwartz <adamberard24@gmail.com>
2021-04-11 13:15:59 -0400

    completed, but dark/light test not passing for some reason

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,19 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [darkMode, setDarkMode] = useState(false)
 
+  function handleClick() {
+    setDarkMode((darkMode) => !darkMode)
+  }
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (darkMode ? "Dark-Mode" : "Light-Mode")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>{darkMode ? "light" : "dark"}</button>
       </header>
       <ShoppingList items={items} />
     </div>
